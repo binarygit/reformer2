7d18aa8557a02be65b8b901b72df54bc4b1dbcca
binarygit <inscription4@outlook.com>
2022-02-26 14:54:22 +0545

    allow mass assignment of :username, :email and :pw

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
     if @user.save
       redirect_to root_path
     else
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
