39247cf9a342a753d9b66274a00070844ac3e70b
binarygit <inscription4@outlook.com>
2022-02-26 14:45:39 +0545

    enable mass assignment of params through user hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(username: params[:username])
+    @user = User.create(user_params)
     if @user.save
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
